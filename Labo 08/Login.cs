ce37ad433e9745f4df497b502913ac663a4e9807
RIADAPLE <00032219@uca.edu.sv>
2020-05-15 13:26:17 -0600

    Subiendo el labo 8

---
+++ Labo 08/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Labo_08
+{
+    public partial class Login : UserControl
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Labo 08/Register.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Labo_08
+{
+    public partial class Register : UserControl
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+    }
+}
